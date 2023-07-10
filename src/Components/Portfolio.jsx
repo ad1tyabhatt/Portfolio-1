6df14425626ff5a79aa06e8b94f8a0a99cf10b8c
Aditya Bhatt <adityabhatt789@gmail.com>
2023-07-10 10:32:55 +0530

    added taskmate and cinemate

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,29 +1,60 @@
-import React from 'react';
-import realestate from '../assets/portfolio/realestate.png'
-const Portfolio = () => {
+import React from "react";
+import realestate from "../assets/portfolio/realestate.png";
+import cinemate from "../assets/portfolio/cinemate.png";
+import taskmate from "../assets/portfolio/taskmate.jpg";
 
-  let demoUrl = "https://propsale.netlify.app/";
-  let githubUrl = "https://github.com/ad1tyabhatt/ecommerce"
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: realestate,
+    },
+    {
+      id: 2,
+      src: cinemate,
+    },
+    {
+      id: 3,
+      src: taskmate,
+    },
+   
+  ];
 
   return (
-    <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-          <p className='py-6'>Check out some of my work here</p>
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-          <div className='shadow-md shadow-gray-400 rounded-lg hover:scale-110 duration-200'>
-            <img src={realestate} alt="img" className='rounded-md duration-200' />
-            <div className='flex items-center justify-center'>
-              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110' onClick={() => window.open(demoUrl, '_blank')}>Demo</button>
-              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110' onClick={() => window.open(githubUrl, '_blank')}>Code</button>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Portfolio;
